1dd48be4d8166ca74b0f2860a29d5c0afa0968b5
jimafisk <jim.fisk@jantcu.com>
2021-11-10 22:05:59 +0000

    Deploying to gh-pages from  @ 944354e59ce952619d7c5da626be40c132f36c3b 🚀

---
+++ spa/ejected/content.js
@@ -0,0 +1,496 @@
+const allContent = [{
+"pager": 1,
+"path": "blog-posts/design-inspiration-the-best-projects-from-december",
+"type": "blog_posts",
+"filename": "design-inspiration-the-best-projects-from-december.json",
+"fields": {
+    "name":"design-inspiration-the-best-projets-from-december",
+    "date": "December 24, 2019",
+    "author": "Alexender Schoitiz",
+    "title": "Design Inspiration: The Best Projects From December",
+    "image": "",
+    "alt": "blog-feature-image",
+    "subtitle": "Black Lines brand identity, by & Smith",
+    "body1": "Black Lines wants it to be as easy to serve a Negroni as it is a pint of lager. The drinks company is seeking to revolutionise the bar experience by serving cocktails by draught with a changing menu of drinks (as well as same favourite stand-bys). A pink grapefruit spritz was served through the summer while a new pear and white tea fizz joins the line-up for winter.",
+    "list": "",
+    "body2": "The company was previously known as Hingston + Co. but has been given a complete rebrand — including a new logo, tap badges, website and branded material — by London-based design studio &amp; Smith. The new identity is based on the Kandinsky abstract painting, Black Lines, and true to its name, is mostly black and white with a few flashes of colour. According to &amp; Smith, the identity brings together “art and science” and has been brought to life through collaborations with nine illustrators.",    
+    "subimage": "blog-image.jpg",
+    "subalt": "blog-details-image-02",
+    "quote": "The public is more familiar with bad design than good design. It is, in effect, conditioned to prefer bad design, because that is what it lives with. The new becomes threatening, the old reassuring.",
+    "signature":"Paul Rand, graphic designer"
+
+}
+},{
+"pager": 1,
+"path": "/",
+"type": "index",
+"filename": "index.json",
+"fields": {
+	"components": [
+		{
+			"name": "hero",
+			"fields": {
+				"background": "/hero-background.jpg",
+				"body": "WE WORK HARD, WE PLAY HARD",
+				"title": "We're a Design Studio That Believes In Great Ideas",
+				"link1": {
+						"title": "See Our Work",
+						"url": "/work"
+					},
+				"link2":
+					{
+						"title": "Connect with us",
+						"url": "/connect"
+					},
+				"image": "/arrow-down.svg"
+			}
+		},
+		{
+			"name": "counter",
+			"fields": {
+				"title": "THE PROOF IS IN THE PUDDING!",
+				"items": [
+					{
+						"body": "Happy Clients",
+						"title": "250+"
+					},
+					{
+						"body": "Project Completed",
+						"title": "100+"
+					},
+					{
+						"body": "Cups of Coffee",
+						"title": "350+"
+					},
+					{
+						"body": "Meetings",
+						"title": "775+"
+					}
+				]
+			}
+		},
+		{
+			"name": "project",
+			"fields":{
+				"title": "OUR RECENT WORK",
+				"body": "Crafting experiences and seeking to make the complex clear & beautiful.",
+				"link": {
+					"title": "MORE WORK",
+					"url": "/"
+				},
+				"items": [
+					{
+						"image": "/project-thumb-four.jpg",
+						"alt": "project-thumb",
+						"title": "PRODUCT LABEL",
+						"body": "KIO-TAPE BRAND",
+						"url": "/"
+					},
+					{
+						"image": "/project-thumb-one.jpg",
+						"title": "UI UX",
+						"body": "USE-LESS BRAND",
+						"url": "/"
+					},					
+					{
+						"image": "/project-thumb-two.jpg",
+						"title": "PRODUCT DESIGN",
+						"body": "OSEN CLOCK",
+						"url": "/"
+					},					
+					{
+						"image": "/project-thumb-three.jpg",
+						"title": "REBRAND",
+						"body": "SEAMLESS WATCH",
+						"url": "/"
+					}
+
+				]
+				
+			}
+
+		},
+		{
+			"name": "testimonial",
+			"fields": {
+				"title": "OPINION FROM REAL PEOPLE",
+				"body": "What clients says about our services.",
+				"items":[
+					{
+						"image": "/user-thumb-one.jpg",
+						"alt": "user-one",
+						"title": "Simonns Hodge",
+						"position": "CEO, Credito",
+						"bio": "Trust us we looked for a very long time and wasted thousands of dollars testing other teams, freelancers, and outsource companies."
+					},
+					{
+						"image": "/user-thumb-two.jpg",
+						"title": "Rekon Montino",
+						"position": "CDO, Bulmuci",
+						"bio": "Roxo products have allowed us to deliver better support to all our clients. The agility with which we can implement new features and workflows is a key element."
+					},
+					{
+						"image": "/user-thumb-three.jpg",
+						"title": "Ryan Hillary",
+						"position": "MD, Udilamo",
+						"bio": "We’re a really busy restaurant, and roxo gives our customers a easy way to order, pay, and pick up their food so they can beat the line and beat that lunch rush."
+					},
+					{
+						"image": "/user-thumb-four.jpg",
+						"title": "Dockrel Kahn",
+						"position": "CPM, Walmet",
+						"bio": "Trust us we looked for a very long time and wasted thousands of dollars testing other teams, freelancers, and outsource companies."
+					},
+					{
+						"image": "/user-thumb-five.jpg",
+						"title": "Orino Monteno",
+						"position": "CEO, Axion",
+						"bio": "Roxo products have allowed us to deliver better support to all our clients. The agility with which we can implement new features and workflows is a key element."
+					},
+					{
+						"image": "/user-thumb-six.jpg",
+						"title": "Welfo Roy",
+						"position": "PDO, Komoyo",
+						"bio": "We’re a really busy restaurant, and roxo gives our customers a easy way to order, pay, and pick up their food so they can beat the line and beat that lunch rush."
+					}
+
+				]
+			}
+		},
+		{
+			"name": "cta",
+			"fields":{
+				"title": "LET'S WORK TOGETHER",
+				"link1": {
+					"title": "Submit Query",
+					"url": "/"
+				},
+				"link2":
+				{
+					"title": "Not Convinced",
+					"url": "/"
+				}
+			}
+		}
+	]
+}
+},{
+"pager": 1,
+"path": "/about",
+"type": "pages",
+"filename": "about.json",
+"fields": {
+	"components": [
+		{
+			"name": "aboutheader",
+			"fields": {
+				"title": "WE ARE ROXO DESIGN. AN AWARD-WINNING CREATIVE STUDIO IN MASSACHUSETTS.",
+				"body": "We are specialized in developing forward-thinking brand identities, websites, illustration and animation for all types of customers. And we do this by bringing our customers through each phase of the design process with us."
+			}
+        },
+		{
+			"name": "expertise",
+			"fields": {
+				"title": "EXPERTISE",
+				"items": {
+
+						"item1": "Customer Experience Design",										
+						"item2": "Digital Products",										
+						"item3": "Development",										
+						"item4": "Campaign & Content",					
+						"item5": "Employer Branding",										
+						"item6": "Animation & Motion Graphics",				
+						"item7": "Packaging & Product Design",			
+						"item8":"Retail & Spacial",
+						"item9":"Print & Editorial Design",
+						"item10":"Concept/Text",
+						"item11":"Information Design"
+					}
+				
+			}
+		},
+		{
+			"name": "grid",
+			"fields": {
+				"title": "WHO TRUSTS OUR JUDGMENT.",
+				"items": {
+					"item1": "/client-logo-one.png",										
+					"item2": "/client-logo-two.png",										
+					"item3": "/client-logo-three.png",										
+					"item4": "/client-logo-four.png",					
+					"item5": "/client-logo-five.png",										
+					"item6": "/client-logo-six.png",				
+					"item7": "/client-logo-seven.png",			
+					"item8":"/client-logo-eight.png",
+					"item9":"/client-logo-nine.png",
+					"item10":"/client-logo-ten.png"
+				}
+			}
+		},
+		{
+			"name": "team",
+			"fields": {
+				"title": "A TEAM THAT MAKES THE DIFFERENCE.",
+				"people": [
+					{
+						"image": "/team-member-one.jpg",
+						"alt": "person",
+						"name": "PABLO MINTO",
+						"title": "Creative Director",
+						"social": [
+							{
+								"icon": "fa fa-medium",
+								"url": "https://www.medium.com"
+							},
+							{
+								"icon": "fa fa-linkedin",
+								"url": "https://linkedin.com"
+							},
+							{
+								"icon": "fa fa-instagram",
+								"url": "https://instagram.com"
+							}
+						]
+
+					},
+					{
+						"image": "/team-member-two.jpg",
+						"alt": "person",
+						"name": "MONTINO RIAU",
+						"title": "Product Manager",
+						"social": [
+							{
+								"icon": "fa fa-medium",
+								"url": "https://www.medium.com"
+							},
+							{
+								"icon": "fa fa-linkedin",
+								"url": "https://linkedin.com"
+							},
+							{
+								"icon": "fa fa-instagram",
+								"url": "https://instagram.com"
+							}
+						]
+
+					},
+					{
+						"image": "/team-member-three.jpg",
+						"alt": "person",
+						"name": "ALEX NAASRI",
+						"title": "Chief Design Officer",
+						"social": [
+							{
+								"icon": "fa fa-medium",
+								"url": "https://www.medium.com"
+							},
+							{
+								"icon": "fa fa-linkedin",
+								"url": "https://linkedin.com"
+							},
+							{
+								"icon": "fa fa-instagram",
+								"url": "https://instagram.com"
+							}
+						]
+
+					},
+					{
+						"image": "/team-member-four.jpg",
+						"alt": "person",
+						"name": "HONGMAN CHIOA",
+						"title": "UX Researcher",
+						"social": [
+							{
+								"icon": "fa fa-medium",
+								"url": "https://www.medium.com"
+							},
+							{
+								"icon": "fa fa-linkedin",
+								"url": "https://linkedin.com"
+							},
+							{
+								"icon": "fa fa-instagram",
+								"url": "https://instagram.com"
+							}
+						]
+
+					},
+					{
+						"image": "/team-member-five.jpg",
+						"alt": "person",
+						"name": "SANTIO ANDRESS",
+						"title": "Content Researcher",
+						"social": [
+							{
+								"icon": "fa fa-medium",
+								"url": "https://www.medium.com"
+							},
+							{
+								"icon": "fa fa-linkedin",
+								"url": "https://linkedin.com"
+							},
+							{
+								"icon": "fa fa-instagram",
+								"url": "https://instagram.com"
+							}
+						]
+
+					},
+					{
+						"image": "/team-member-six.jpg",
+						"alt": "person",
+						"name": "RAMESH PAUL",
+						"title": "Creative Designer",
+						"social": [
+							{
+								"icon": "fa fa-medium",
+								"url": "https://www.medium.com"
+							},
+							{
+								"icon": "fa fa-linkedin",
+								"url": "https://linkedin.com"
+							},
+							{
+								"icon": "fa fa-instagram",
+								"url": "https://instagram.com"
+							}
+						]
+
+					}
+					
+				]	
+			}
+		},
+		{
+			"name": "cta",
+			"fields":{
+				"title": "LET'S WORK TOGETHER",
+				"link1": {
+					"title": "Submit Query",
+					"url": "/"
+				},
+				"link2":
+				{
+					"title": "Not Convinced",
+					"url": "/"
+				}
+			}
+		}	
+    ]	
+}
+
+
+},{
+"pager": 1,
+"path": "/blog",
+"type": "pages",
+"filename": "blog.json",
+"fields": {
+    "components": [
+        {
+            "name": "blog",
+            "fields": {
+                "title": "Roxo Blog",
+                "desc": "By fusing strategy & design we help our partners build their brands, drive business, & stand out from the noise in saturated markets! Follow our blog for the latest case studies and projects."
+            }   
+        },
+		{
+			"name": "cta",
+			"fields":{
+				"title": "LET'S WORK TOGETHER",
+				"link1": {
+					"title": "Submit Query",
+					"url": "/"
+				},
+				"link2":
+				{
+					"title": "Not Convinced",
+					"url": "/"
+				}
+			}
+		}	
+    ]
+}
+},{
+"pager": 1,
+"path": "/portfolio",
+"type": "pages",
+"filename": "portfolio.json",
+"fields": {
+	"components": [
+		{
+			"name": "projectheader",
+			"fields": {
+				"title1": "We build better ",
+                "title2":"products",
+                "title3":" to help our clients build better",
+                "title4":" companies",
+				"body": "Scroll Down",
+                "image":"/arrow-down.svg",
+                "alt": "arrow down"
+			}
+        },
+        {
+			"name": "project",
+			"fields":{
+				"title": "OUR RECENT WORK",
+				"body": "Crafting experiences and seeking to make the complex clear & beautiful.",
+				"link": {
+					"title": "MORE WORK",
+					"url": "/"
+				},
+				"items": [
+					{
+						"image": "/project-thumb-four.jpg",
+						"alt": "project-thumb",
+						"title": "PRODUCT LABEL",
+						"body": "KIO-TAPE BRAND",
+						"url": "/"
+					},
+					{
+						"image": "/project-thumb-one.jpg",
+						"title": "UI UX",
+						"body": "USE-LESS BRAND",
+						"url": "/"
+					},					
+					{
+						"image": "/project-thumb-two.jpg",
+						"title": "PRODUCT DESIGN",
+						"body": "OSEN CLOCK",
+						"url": "/"
+					},					
+					{
+						"image": "/project-thumb-three.jpg",
+						"title": "REBRAND",
+						"body": "SEAMLESS WATCH",
+						"url": "/"
+					}
+
+				]
+				
+			}
+
+        },
+        {
+			"name": "cta",
+			"fields":{
+				"title": "LET'S WORK TOGETHER",
+				"link1": {
+					"title": "Submit Query",
+					"url": "/"
+				},
+				"link2":
+				{
+					"title": "Not Convinced",
+					"url": "/"
+				}
+			}
+		}	
+
+    ]
+}
+
+},];
+
+export default allContent;
