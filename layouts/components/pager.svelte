0a7d09ce5203d72ad5fce24c4348ee6e37d82355
jimafisk <jimafisk@gmail.com>
2021-12-28 16:59:43 -0500

    Define upper and lower limits of pager.

--- layouts/components/pager.svelte
+++ layouts/components/pager.svelte
@@ -4,21 +4,40 @@
 
 <div class="site-blog-pagination"> 
     <ul class="pagination">
-        <li class="page-item">
-            <a href="/blog/" class="page-link" aria-label="First"><span aria-hidden="true">««</span></a>
-        </li>
-        <li class="page-item disabled">
-        <a class="page-link" aria-label="Previous"><span aria-hidden="true">«</span></a>
-        </li>
+        {#if currentPage > 1}
+            <li class="page-item">
+                <a href="/blog/" class="page-link" aria-label="First"><span aria-hidden="true">««</span></a>
+            </li>
+            <li class="page-item">
+                <a href="blog/{currentPage - 1}" class="page-link" aria-label="Previous"><span aria-hidden="true">«</span></a>
+            </li>
+        {:else}
+            <li class="page-item disabled">
+                <a href="blog" class="page-link" aria-label="First"><span aria-hidden="true">««</span></a>
+            </li>
+            <li class="page-item disabled">
+                <a href="blog/{currentPage - 1}" class="page-link" aria-label="Previous"><span aria-hidden="true">«</span></a>
+            </li>
+        {/if}
+
         <li class="page-item active"><a class="page-link" href="/blog/">1</a></li>
         <li class="page-item"><a class="page-link" href="/blog/page/2/">2</a></li>
-        <li class="page-item">
-        <a href="/blog/page/2/" class="page-link" aria-label="Next"><span aria-hidden="true">»</span></a>
-        </li>
-        
-        <li class="page-item">
-            <a href="/blog/page/2/" class="page-link" aria-label="Last"><span aria-hidden="true">»»</span></a>
-        </li>
+
+        {#if currentPage < totalPages}
+            <li class="page-item">
+                <a href="blog/{currentPage + 1}" class="page-link" aria-label="Next"><span aria-hidden="true">»</span></a>
+            </li>
+            <li class="page-item">
+                <a href="blog/{totalPages}" class="page-link" aria-label="Last"><span aria-hidden="true">»»</span></a>
+            </li>
+        {:else}
+            <li class="page-item disabled">
+                <a href="blog/{currentPage + 1}" class="page-link" aria-label="Next"><span aria-hidden="true">»</span></a>
+            </li>
+            <li class="page-item disabled">
+                <a href="blog/{totalPages}" class="page-link" aria-label="Last"><span aria-hidden="true">»»</span></a>
+            </li>
+        {/if}
     </ul>
 </div>
 
